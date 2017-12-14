d3ebd0f2ca90681c5e8cbb3499c5f2c770dda338
darkgray <jorgegaguilar@hotmail.es>
2017-12-14 18:10:16 +0100

    Vuelve a funcionar

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,6 +1,5 @@
 package hello;
 
-asdfasdf
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
