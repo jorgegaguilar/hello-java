5467393339574cdf05897db24cfb569a6a6e43f6
darkgray <jorgegaguilar@hotmail.es>
2017-12-14 18:32:50 +0100

    Cambios

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -13,7 +13,7 @@ public class GreetingController {
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, 
     					   @RequestParam(value="lang", required=false, defaultValue="en") String lang, 
     					   Model model) {
-    	model.addAttribute("hello", GreetingTranslator.sayHelloIn(lang));
+    	model.addAttribute("hello", greetingTranslator.sayHelloIn(lang));
         model.addAttribute("name", name);
         return "greeting";
     }
